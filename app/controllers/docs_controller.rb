ca5fe534220af4aa51989fc058bb59ea69b0983c
Saliha Ouannoughi <maybenot@pt.lu>
2018-02-21 14:10:43 +0100

    added docs controller

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,22 @@
+class DocsController < ApplicationController
+	def new
+		#...
+	end
+	def show
+		#...
+	end
+	def edit
+		#...
+	end
+	def update
+		#...
+	end
+	def destroy
+		#...
+	end
+	private	
+	def find_doc
+	end
+	def doc_params
+	end
+end

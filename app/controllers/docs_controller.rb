1a423537e77cfdae77165c8155f819f94083b598
Saliha Ouannoughi <maybenot@pt.lu>
2018-02-22 11:05:11 +0100

    install devise module

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,22 +1,73 @@
 class DocsController < ApplicationController
-	def new
-		#...
-	end
-	def show
-		#...
-	end
-	def edit
-		#...
-	end
-	def update
-		#...
-	end
-	def destroy
-		#...
-	end
-	private	
-	def find_doc
-	end
-	def doc_params
-	end
+
+ before_action :find_doc, only: [:show, :edit, :update, :destroy]  
+
+ def index
+
+   @docs = Doc.all
+
+ end  
+
+ def show
+
+ end  
+
+ def new
+
+   @doc = Doc.new
+
+ end  
+
+ def update
+
+   if @doc.update(doc_params)
+
+     redirect_to @doc
+
+   else
+
+     render 'edit'
+
+   end
+
+ end    
+
+ def destroy
+
+     @doc.destroy
+
+     redirect_to docs_path
+
+   end  
+
+   def create
+
+   @doc = Doc.new(doc_params)
+
+   if @doc.save
+
+     redirect_to @doc
+
+   else
+
+     render 'new'
+
+   end
+
+ end  
+
+ private  
+
+ def find_doc
+
+   @doc = Doc.find(params[:id])
+
+ end  
+
+ def doc_params
+
+   params.require(:doc).permit(:title, :content)
+
+ end
+
 end
